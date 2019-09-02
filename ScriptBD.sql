fa233890267bbfdc3cfa5a5ffbb7f8678cb763dd
AdrianLovo <adrianhumbertocastillo@gmail.com>
2019-09-01 21:38:26 -0600

    Se agrego script para creacion de BD

---
+++ ScriptBD.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE login
+GO
+USE login
+GO
+CREATE TABLE IF NOT EXISTS usuario(
+    idUsuario INT NOT NULL AUTO_INCREMENT COMMENT 'Clave primaria',
+    usuario VARCHAR(50),
+    password VARCHAR(50),
+    email VARCHAR(50),
+    intentos INT,
+    PRIMARY KEY (idUsuario)
+)
+GO
+CREATE USER 'usuario'@'localhost' IDENTIFIED BY ''
+GO
+GRANT ALL PRIVILEGES ON * . * TO 'usuario'@'localhost'
+GO
